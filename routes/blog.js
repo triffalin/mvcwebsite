6c64c92649d277bdb1764a346ea63fcb67a7f0b7
Alin Triff <triffalin@gmail.com>
2023-10-11 16:08:56 +0300

    added update and delete functionality to the model

--- routes/blog.js
+++ routes/blog.js
@@ -69,7 +69,7 @@ router.get('/posts/:id/edit', async function (req, res) {
 	const post = await db.getDb().collection('posts').findOne({ _id: postId });
 
 	if (!post) {
-		return res.render('404'); // 404.ejs is missing at this point - it will be added later!
+		return res.render('404');
 	}
 
 	let sessionInputData = req.session.inputData;
@@ -94,7 +94,6 @@ router.get('/posts/:id/edit', async function (req, res) {
 router.post('/posts/:id/edit', async function (req, res) {
 	const enteredTitle = req.body.title;
 	const enteredContent = req.body.content;
-	const postId = new ObjectId(req.params.id);
 
 	if (
 		!enteredTitle ||
@@ -113,21 +112,15 @@ router.post('/posts/:id/edit', async function (req, res) {
 		return;
 	}
 
-	await db
-		.getDb()
-		.collection('posts')
-		.updateOne(
-			{ _id: postId },
-			{ $set: { title: enteredTitle, content: enteredContent } }
-		);
+	const post = new Post(enteredTitle, enteredContent, req.params.id);
+	await post.save();
 
 	res.redirect('/admin');
 });
 
 router.post('/posts/:id/delete', async function (req, res) {
-	const postId = new ObjectId(req.params.id);
-	await db.getDb().collection('posts').deleteOne({ _id: postId });
-
+	const post = new Post(null, null, req.params.id);
+	await post.delete();
 	res.redirect('/admin');
 });
 
