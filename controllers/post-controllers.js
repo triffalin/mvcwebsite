9a00cea7cdf1594046f33315921831745cf7268a
Alin Triff <triffalin@gmail.com>
2023-10-12 19:05:35 +0300

    refactored the sessions and input validation errors functionality

--- controllers/post-controllers.js
+++ controllers/post-controllers.js
@@ -1,4 +1,6 @@
 const Post = require('../models/post');
+const validationSession = require('../util/validation-session');
+const validation = require('../util/validation');
 
 function getHome(req, res) {
 	res.render('welcome', { csrfToken: req.csrfToken() });
@@ -11,21 +13,14 @@ async function getAdmin(req, res) {
 
 	const posts = await Post.fetchAll();
 
-	let sessionInputData = req.session.inputData;
-
-	if (!sessionInputData) {
-		sessionInputData = {
-			hasError: false,
-			title: '',
-			content: '',
-		};
-	}
-
-	req.session.inputData = null;
+	sessionErrorData = validationSession.getSessionErrorData(req, {
+		title: '',
+		content: '',
+	});
 
 	res.render('admin', {
 		posts: posts,
-		inputData: sessionInputData,
+		inputData: sessionErrorData,
 		csrfToken: req.csrfToken(),
 	});
 }
@@ -34,20 +29,19 @@ async function createPost(req, res) {
 	const enteredTitle = req.body.title;
 	const enteredContent = req.body.content;
 
-	if (
-		!enteredTitle ||
-		!enteredContent ||
-		enteredTitle.trim() === '' ||
-		enteredContent.trim() === ''
-	) {
-		req.session.inputData = {
-			hasError: true,
-			message: 'Invalid input - please check your data.',
-			title: enteredTitle,
-			content: enteredContent,
-		};
+	if (!validation.postIsValid(enteredTitle, enteredContent)) {
+		validationSession.flashErrorsToSession(
+			req,
+			{
+				message: 'Invalid input - please check your data.',
+				title: enteredTitle,
+				content: enteredContent,
+			},
+			function () {
+				res.redirect('/admin');
+			}
+		);
 
-		res.redirect('/admin');
 		return; // or return res.redirect('/admin'); => Has the same effect
 	}
 
@@ -65,21 +59,14 @@ async function getSinglePost(req, res) {
 		return res.render('404');
 	}
 
-	let sessionInputData = req.session.inputData;
-
-	if (!sessionInputData) {
-		sessionInputData = {
-			hasError: false,
-			title: post.title,
-			content: post.content,
-		};
-	}
-
-	req.session.inputData = null;
+	sessionErrorData = validationSession.getSessionErrorData(req, {
+		title: post.title,
+		content: post.content,
+	});
 
 	res.render('single-post', {
 		post: post,
-		inputData: sessionInputData,
+		inputData: sessionErrorData,
 		csrfToken: req.csrfToken(),
 	});
 }
@@ -88,20 +75,19 @@ async function updatePost(req, res) {
 	const enteredTitle = req.body.title;
 	const enteredContent = req.body.content;
 
-	if (
-		!enteredTitle ||
-		!enteredContent ||
-		enteredTitle.trim() === '' ||
-		enteredContent.trim() === ''
-	) {
-		req.session.inputData = {
-			hasError: true,
-			message: 'Invalid input - please check your data.',
-			title: enteredTitle,
-			content: enteredContent,
-		};
+	if (!validation.postIsValid(enteredTitle, enteredContent)) {
+		validationSession.flashErrorsToSession(
+			req,
+			{
+				message: 'Invalid input - please check your data.',
+				title: enteredTitle,
+				content: enteredContent,
+			},
+			function () {
+				res.redirect(`/posts/${req.params.id}/edit`);
+			}
+		);
 
-		res.redirect(`/posts/${req.params.id}/edit`);
 		return;
 	}
 
